29cebad8620878a94e0827f6170a95d4287a5754
Hoon <avbalonzo30@gmail.com>
2025-10-17 15:51:28 +0800

    created the footer

---
+++ resources/views/partials/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer footer-center p-4 bg-primary-bg text-text-muted border-t border-border-color">
+    <div>
+        <p>© {{ date('Y') }} Movie Recommendation System • Created by JMR</p>
+    </div>
+</footer>
