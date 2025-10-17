fb6be13227e69fa9c6f0a05c1f69ff7fdb26cab7
Hoon <avbalonzo30@gmail.com>
2025-10-17 15:54:10 +0800

    added the fields for fetching the movie details

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    protected $connection = 'mysql_movies'; 
+    protected $table = 'movies';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'release_year',
+        'poster_url',
+        'background_url',
+        'trailer_url',
+        'country_id',
+        'language_id',
+    ];
+
+    public $timestamps = false;
 }
