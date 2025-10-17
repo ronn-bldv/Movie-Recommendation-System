7b1a7e778d159d9d63d59cafeceaccab6d3d9d06
Hoon <avbalonzo30@gmail.com>
2025-10-17 15:47:10 +0800

    updated the routes needed

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,19 @@
 use App\Http\Controllers\Socialite\ProviderCallbackController;
 use App\Http\Controllers\Socialite\ProviderRedirectController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MovieController::class, 'index'])
+    ->name('home');
+
+// Route::get('/', function () {
+//     return view('home', ['user' => Auth::user()]);
+// })->name('home');
+
+
+//hindi ko sure if pano yung gagawin here sa login HAHAHAHAAAHAH, may error after login if hindi third party ginamit
 
 Route::get('/auth', function () {
     return view('auth');
@@ -20,9 +27,6 @@ Route::get('/auth/{provider}/redirect', ProviderRedirectController::class)->name
 // Callback from provider
 Route::get('/auth/{provider}/callback', ProviderCallbackController::class)->name('auth.callback');
 
-Route::get('/home', function () {
-    return view('home', ['user' => Auth::user()]);
-})->name('home');
 
 Route::post('/register', [AuthController::class, 'register'])
     ->name('register');
@@ -35,3 +39,18 @@ Route::post('/logout', [AuthController::class, 'logout'])
 
 Route::get('/login', [AuthController::class, 'login'])
     ->name('login');
+
+
+Route::get('/viewMovie/{id}', [MovieController::class, 'show'])->name('movie.show');
+
+Route::get('/movie/add', function () {
+    return view('pages.manage-movie', ['editing' => false]);
+})->name('movie.add');
+
+Route::get('/movie/edit/{id}', function ($id) {
+    return view('pages.manage-movie', ['editing' => true, 'movieId' => $id]);
+})->name('movie.edit');
+
+Route::get('/profile', function () {
+    return view('pages.profile');
+})->name('profile');
